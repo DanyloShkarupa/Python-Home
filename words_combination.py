3b4a9bbb6d824ed1ad1f36df38ee8b642363b369
DanyloShkarupa <104980480+DanyloShkarupa@users.noreply.github.com>
2022-05-19 14:03:55 +0300

    Update words_combination.py

--- words_combination.py
+++ words_combination.py
@@ -7,7 +7,7 @@ while len(s_new) < 5:
     line = s[random.randint(0, len(s) - 1)]
     if line not in s_new:
         s_new += line
-    elif s.count(line) > 1 and s_new.count(line) <= s.count(line):
+    elif s.count(line) > 1 and s_new.count(line) < s.count(line):
         s_new += line
 print(s_new)
 
